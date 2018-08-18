524d1d6886d3ad7f463401ce9c7a5a01c67a801a
wangxuebin992134 <416003592@qq.com>
2018-08-18 21:54:08 +0800

    index后台基础功能完成

--- public/back/js/common.js
+++ public/back/js/common.js
@@ -10,3 +10,38 @@
     NProgress.done();
    },500)
  })
+
+
+$(function(){
+ //1.分类管理的切换功能
+ $(".nav .category").click(function(){
+    $(".nav .child").slideToggle();
+    
+ })
+
+
+   //2.点击右侧主体部分切换菜单切换左侧栏
+ $(".lt_main .icon_menu").click(function(){
+  $(".lt_aside").stop().toggleClass("hidemenu");
+  $(".lt_main").stop().toggleClass("hidemenu");
+  $(".lt_topbar").stop().toggleClass("hidemenu");
+})
+//3.点击topbar退出按钮，显示模态框
+  $(".icon_logout").click(function(){
+    $("#logoutModal").modal("show");
+  })
+  $(".logoutBtn").click(function(){
+      $.ajax({
+        type:"get",
+        url:"/employee/checkRootLogin",
+        dataType:"json",
+        success:function(info){
+            // console.log(info);
+            if(info.success){
+              location.href="login.html";
+            }
+          
+        }
+      })
+  })
+})
