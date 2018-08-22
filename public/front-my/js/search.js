f5ee0e169ba4c15258e15fcfce18dbf613127058
wangxuebin992134 <416003592@qq.com>
2018-08-23 01:02:00 +0800

    搜索列表-静态页面，基本渲染，搜索功能，添加提示框，基本排序，添加加载中效果完成

--- public/front-my/js/search.js
+++ public/front-my/js/search.js
@@ -1,107 +1,112 @@
-$(function(){
+$(function () {
   //要进行localStorage本地存储进行历史记录管理
-   // 准备假数据: 将下面三行代码, 在控制台执行, 可以添加假数据
+  // 准备假数据: 将下面三行代码, 在控制台执行, 可以添加假数据
   // var arr = [ "耐克", "阿迪", "阿迪王", "耐克王", "新百伦" ];
   // var jsonStr = JSON.stringify( arr );
   // localStorage.setItem( "search_list", jsonStr );
 
   //1.从本地存储读取历史记录,读取的是一个json字符串
-    function getHistory(){
-      var history = localStorage.getItem("search_list")||'[]';
-      //转成数组
-      var arr = JSON.parse(history);
-      // console.log(arr);
-      return arr;
+  function getHistory() {
+    var history = localStorage.getItem("search_list") || '[]';
+    //转成数组
+    var arr = JSON.parse(history);
+    // console.log(arr);
+    return arr;
 
-    }
-   
-    
+  }
+
+
   //  console.log(getHistory());
-   
-   function render(){
-     var arr = getHistory();
-     var htmlStr = template("historyTpl",{arr:arr});
-     $(".lt_history").html(htmlStr);
-  
-     
-   }
-   render();
-   //功能2:清空历史记录
-   //注册委托事件
-   $(".lt_history").on("click",".btn-empty",function(){
-       //添加mui确认框
-       //参数message,title,btnValue
-       mui.confirm("你确定要清空历史记录嘛?","温馨提示",["取消","确认"],function(e){
-          // console.log(e);
-          if(e.index === 1){
-            localStorage.removeItem("search_list");
-            render();
-          }
-          
-       }) 
 
-   })
-   
-   //功能3：删除单条历史记录
-   $(".lt_history").on("click",".btn_del",function(){
-      //获取单条删除按钮的对应的index
-      var index = $(this).data("index");
-      // console.log(index);
-      //获取本地存储的历史数据
-     var arr = getHistory();
-     //删除对应的数组中对应的项
-     arr.splice(index,1);
-    //  console.log(arr);
-     
-     //转成json字符串存储到本地储存
-     localStorage.setItem("search_list",JSON.stringify(arr));
-     //页面重新渲染
-     render();
-     
+  function render() {
+    var arr = getHistory();
+    var htmlStr = template("historyTpl", { arr: arr });
+    $(".lt_history").html(htmlStr);
 
-      
-   })
-   // 4.添加历史记录
+
+  }
+  render();
+  //功能2:清空历史记录
+  //注册委托事件
+  $(".lt_history").on("click", ".btn-empty", function () {
+    //添加mui确认框
+    //参数message,title,btnValue
+    mui.confirm("你确定要清空历史记录嘛?", "温馨提示", ["取消", "确认"], function (e) {
+      // console.log(e);
+      if (e.index === 1) {
+        localStorage.removeItem("search_list");
+        render();
+      }
+
+    })
+
+  })
+
+  //功能3：删除单条历史记录
+  $(".lt_history").on("click", ".btn_del", function () {
+    mui.confirm("你确定要删除该条记录嘛?", "温馨提示", ["取消", "确认"], function (e) {
+      if (e.index === 1) {
+        //获取单条删除按钮的对应的index
+        var index = $(this).data("index");
+        // console.log(index);
+        //获取本地存储的历史数据
+        var arr = getHistory();
+        //删除对应的数组中对应的项
+        arr.splice(index, 1);
+        //  console.log(arr);
+
+        //转成json字符串存储到本地储存
+        localStorage.setItem("search_list", JSON.stringify(arr));
+        //页面重新渲染
+        render();
+      }
+    })
+
+
+
+
+  })
+  // 4.添加历史记录
   //  点击搜索按钮添加点击事件
-  $(".search_btn").click(function(){
-   //获取input框里的值
+  $(".search_btn").click(function () {
+    //获取input框里的值
     var key = $(".search_input").val().trim();
-    if(key === ""){
+    if (key === "") {
       alert("请输入搜索关键字");
     }
     //从本地存储获取历史记录
-     var arr = getHistory();
+    var arr = getHistory();
 
-     
-    
-  
+
+
+
     //判断有没有重复项,删除重复项
-    
-     var index = arr.indexOf(key);
-     if(index != -1){
-       //说明有重复项
-       arr.splice(index,1)
-     }
-     //数组的长度限制
-     if(arr.length >=6){
-       arr.pop();
-     }
-     //添加数组的最前面
-     arr.unshift(key);
-    //转成json字符串存在本地存储
-    localStorage.setItem("search_list",JSON.stringify(arr));
-     render();
-     //重置input框里文本
-     $(".search_input").val("");
-     //点击搜索按钮跳转商品列表页
-     location.href = "searchList.html?key="+ key;
 
-     
-     
+    var index = arr.indexOf(key);
+    if (index != -1) {
+      //说明有重复项
+      arr.splice(index, 1)
+    }
+    //数组的长度限制
+    if (arr.length >= 6) {
+      arr.pop();
+    }
+    //添加数组的最前面
+    arr.unshift(key);
+    //转成json字符串存在本地存储
+    localStorage.setItem("search_list", JSON.stringify(arr));
+    render();
+    //重置input框里文本
+    $(".search_input").val("");
+    //点击搜索按钮跳转商品列表页
+    location.href = "searchList.html?key=" + key;
+
+
+
 
   })
-  
-  
-  
-      
+
+
+
+
 })
--- public/front-my/js/searchList.js
+++ public/front-my/js/searchList.js
@@ -4,9 +4,83 @@ $(function(){
     // console.log(key);
   $(".search_input").val(key);
   render();
+ //2.点击搜索按钮，实现搜索功能
+   $(".search_btn").click(function(){
+      var key = $(".search_input").val();
+      if( key.trim()===""){
+        mui.toast("请输入搜索关键字");
+       return;
+      }
+      render();
+    //如果有搜索关键字，就添加到本地储存中
+     var history = localStorage.getItem("search_list")||'[]';
+     var arr  = JSON.parse(history);
+     //判断数组有没有重复项
+     var index = arr.indexOf(key);
+     if(index != -1){
+       //说明有重复项
+       arr.splice(index,1);
+     }
+     if(arr.length >= 6){
+        arr.pop();
+     }
+     //添加数组到最前面
+     arr.unshift(key);
+     //转成字符串存到本地存储
+     localStorage.setItem("search_list",JSON.stringify(arr));
+    //  console.log(arr);
+     
+   })
 
-  
+   //3.添加排序功能(点击切换类)
+   $(".lt_sort a[data-type]").click(function(){
+      
+      //如果之前有current类的,切换箭头
+      if($(this).hasClass("current")){
+          $(this).find("i").toggleClass("fa-angle-up").toggleClass("fa-angle-down");
+      }
+      $(this).addClass("current").siblings().removeClass("current");
+      render();
+      
+
+   })
+
+
+
   function render(){
+     //准备请求数据，渲染时，显示加载中的效果
+      $(".lt_product").html('<div class="loading"></div>');
+      //在render方法里面处理所有参数
+      //必传的3个参数
+      var params = {}
+      params.proName = $(".search_input").val();
+      params.page = 1;
+      params.pageSize=100;
+      //可传可不传的参数
+      //判断有没有判断高亮元素，判断是否传参
+      //根据箭头的方向来判断是升序还是降序
+      var $current = $(".lt_sort a.current");
+      if($current.length>0){
+       var sortName =  $current.data("type");
+       var sortValue = $current.find("i").hasClass("fa-angle-down")?2:1;
+       params[sortName] = sortValue;
+      }
 
+      // params.price
+      console.log(params);
+    setTimeout(function(){
+      $.ajax({
+        type:"get",
+        url:"/product/queryProduct",
+        data:params,
+        dataType:"json",
+        success:function(info){
+            console.log(info);
+            var htmlStr = template("productTpl",info);
+            $(".lt_product").html(htmlStr);
+            
+        }
+      })
+    },500)
   }
 })
